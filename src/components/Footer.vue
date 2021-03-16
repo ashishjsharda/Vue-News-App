973bfbe3dc1c7172047735e3343aabf5436f4b88
Ashish Sharda <ashishjsharda@gmail.com>
2021-03-15 20:26:00 -0400

    Add all my files

---
+++ src/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+  <footer
+    class="px-4 py-8 text-sm font-bold text-center text-blue-100 bg-blue-900">
+    <p class="text-sm tracking-wide">Copyright (c) Ashish</p>
+  </footer>
+</template>
